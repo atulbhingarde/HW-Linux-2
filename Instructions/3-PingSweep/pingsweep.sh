3460e3075ee5af233297a06f6139f3a9d02d2848
atulbhingarde <atul.bhingarde@ge.com>
2019-10-14 23:34:10 -0400

    final list of ips down or up

--- Instructions/3-PingSweep/pingsweep.sh
+++ Instructions/3-PingSweep/pingsweep.sh
@@ -2,12 +2,14 @@
 # define constant 
 # get the param count in  variable
 export paramCnt=$#
+#touch up_list down_list
+
 if [ ${paramCnt} == 1 ]; then
     export PREFIX=$1
-    export count=5
+    export count=255
     for Suffix in `seq ${count}` ; do
         export TARGET=${PREFIX}"."${Suffix}
-        ( ping -c 1 ${TARGET} > /dev/null 2>&1 ) && echo "${TARGET} is up "  || echo "${TARGET} is down"
+        ( ping -c 1 ${TARGET} > /dev/null 2>&1 ) && ( echo "${TARGET} is up " | tee -a up_list )  || ( echo "${TARGET} is down" | tee -a down_list ) 
     done
 else
     echo "required parameter is not passed to the script "
